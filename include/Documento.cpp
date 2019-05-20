3105a8d676c1fa8658e7680fd21304ca2f5c04d5
João Victor de Castro <joaovictordecastroa@gmail.com>
2019-05-20 16:45:00 -0300

    Adição das condições de caractéres especiais

---
+++ include/Documento.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <Documento.h>
+#include <Palavra.h>
+
+using namespace std;
+
--- src/Palavra.cpp
+++ src/Palavra.cpp
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string>
 
-
+//ISO 8859-1
 char* removerAcento( char* str ) {
     char *p = str;
     while ( (*p)!=0 ) {
@@ -24,24 +24,45 @@ Palavra::Palavra(string x){
 string Palavra::converter(const string &x){
     string y = x;
     for (int i = 0; i < y.length(); i++){
-
-        if(iswgraph(y[i])){
-            y[i] = tolower(y[i]);
-            y[i] = *removerAcento(&y[i]);
+        if (y[i] >= 128 && y[i] <= 159){
+            y.erase(i);
+        }
+        if (y[i] >= 161 && y[i] <= 191){
+            y.erase(i);
+        }
+        if (y[i] >= 247 && y[i] <= 248){
+            y.erase(i);
+        }
+        if (y[i] >= 253 && y[i] <= 255){
+            y.erase(i);
+        }
+        if (y[i] >= 58 && y[i] <= 64){
+            y.erase(i);
+        }
+        if (y[i] >= 91 && y[i] <= 96){
+            y.erase(i);
+        }
+        if (y[i] >= 123 && y[i] <= 126){
+            y.erase(i);
         }
 
-        else y.erase(i);
-
+        y[i] = tolower(y[i]);
+        y[i] = *removerAcento(&y[i]);
     }
     return y;
 }
 
 string Palavra::paraString(){
     return this->palavra_;
-
+}
 
 bool Palavra::operator==(const Palavra &x){
-    return(this->palavra_ == x.palavra_);
+    if (this -> palavra_ == x.palavra_){
+        return true;
+    }else {
+        return false;
+    }
+    
 }
 
 void Palavra::operator=(const Palavra &x){
