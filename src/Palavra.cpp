9de1c8da21375bb10508230a3af1298efb45d377
João Victor de Castro <joaovictordecastroa@gmail.com>
2019-05-21 15:58:07 -0300

    Atualização do método converter

--- src/Palavra.cpp
+++ src/Palavra.cpp
@@ -27,32 +27,10 @@ string Palavra::converter(const string &x){
     for (int i = 0; i < y.length(); i++){
         y[i] = tolower(y[i]);
         y[i] = *removerAcento(&y[i]);
-        
-        /*if (y[i] >= 33 && y[i] <= 47){
-            std::cout<< y[i] << std::endl;
-            y[i]= '\0';
-        }else if (y[i] >= 128 && y[i] <= 159){
-            y.erase(i); 
-        }else if (y[i] >= 161 && y[i] <= 191){
-            y.erase(i);
-        }else if (y[i] >= 247 && y[i] <= 248){
-            y.erase(i);
-        }else if (y[i] >= 253 && y[i] <= 255){
-            y.erase(i);
-        }else if (y[i] >= 58 && y[i] <= 64){
-            y.erase(i);
-        }else if (y[i] >= 91 && y[i] <= 96){
-            y.erase(i);
-        }else if (y[i] >= 123 && y[i] <= 126){
-            y.erase(i);
-        }else {
-            y[i];
-        }*/
-
         if (!(y[i] >= 48 && y[i] <= 57)){
             if (!(y[i] >= 65 && y[i] <= 90)){
                 if (!(y[i] >= 97 && y[i] <= 122)){
-                    y[i] = '\0';
+                    y.erase(i);
                 }
             }
         }
