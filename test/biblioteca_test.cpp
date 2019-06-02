e1826011d423ecd40b2af6002f23c193f9c1123a
Mariana Leite <marianaleite785@gmail.com>
2019-06-02 14:41:11 -0300

    Separação em pastas de arquivos de teste, src e include; inclusão do arquivo settings.json para padronização do encoding

---
+++ test/biblioteca_test.cpp
@@ -0,0 +1,6 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../include/doctest.h"
+
+TEST_SUITE("Testes sobre os métodos da classe Biblioteca"){
+
+}
--- test/main_test.cpp
+++ test/main_test.cpp
@@ -2,15 +2,15 @@
 #include "../include/Documento.h"
 #include "../include/Palavra.h"
 #include <iostream>
+
 int main(){
     vector<string> resultado;
     string argBusca;
-    Biblioteca a("dirBusca");
+    Biblioteca a("diretorio");
     cout<<"Entre com os argumentos da busca:"<<endl;
     getline(cin,argBusca);
     resultado = a.busca(argBusca);
     for (string s:resultado){
         cout << s << endl;
     }
-
 }
