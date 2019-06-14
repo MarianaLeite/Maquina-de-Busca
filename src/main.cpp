ba95b660002644d8a72ac7ba735128b019a70a7e
kelvin viveiros <kelvinviveiros@gmail.com>
2019-06-13 21:15:03 -0300

    main final stage

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,23 @@
 using namespace std;
 
 int main(){
-    Biblioteca biblioteca ("test/diretorio");
-    vector<string> documentos = biblioteca.busca("A");
-    cout << endl <<"____________________________________________________________________________" << endl;
-    cout << endl <<"Ranking dos Documentos:" << endl;
-    cout << endl << "A busca de A na Biblioteca" << endl;
-    for (int i = 0; i < documentos.size(); i++){
-        Documento documento = documentos[i];
-        cout << endl << i + 1 << ") " <<documentos[i];
-    }
-    cout << endl <<"____________________________________________________________________________" << endl;
-    cout << endl << "Para casos mais complexos iremos utilizar um paragrafo de um texto como exemplo:" << endl;
-    Biblioteca bibliotecaComplexa ("documentosExemplo");
-    vector<string> documentosComplexos = bibliotecaComplexa.busca("pensando");
-    cout << endl <<"Ranking dos Documentos:" << endl;
-    for (int i = 0; i < documentosComplexos.size(); i++){
-        Documento documentoComplexo = documentosComplexos[i];
-        cout << endl << i + 1 << ") " <<documentosComplexos[i];
-    }
-    cout << endl <<"____________________________________________________________________________" << endl;
-    
+    string caminho;
+    string busca;
+    cout << "Digite o caminho completo (path) onde se encontra seus arquivos: " << endl;
+    cin >> caminho;
+    Biblioteca biblioteca(caminho);
 
+    while(1){
+        cout << "Digite sua busca: " << endl;
+        cin >> busca;
+        cout << endl <<"____________________________________________________________________________" << endl;
+        vector<string> documentos = biblioteca.busca(busca);
+        cout << endl <<"Ranking dos Documentos:" << endl;
+        for (int i = 0; i < documentos.size(); i++){
+            Documento documentoComplexo = documentos[i];
+            cout << endl << i + 1 << ") " <<documentos[i];
+        }
+        cout << endl <<"____________________________________________________________________________" << endl;
+    }
 }
     
